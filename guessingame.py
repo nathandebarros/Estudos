3f4b472193de19800b745cb919d3a98e496c2708
nathandebarros <nathanbartolomeu@gmail.com>
2026-09-09 12:26:20 -0300

    Update guessingame.py

--- guessingame.py
+++ guessingame.py
@@ -1,1 +1,14 @@
-print("Hello, World!")
+import random
+
+ngerado = random.randrange(1, 10)
+
+nescolhido = int(input("Escolha um número: "))
+
+while(ngerado != nescolhido):
+    if nescolhido < ngerado:
+        nescolhido = int(input("Escolha errada. Tente um número maior: "))
+    else:
+        nescolhido = int(input("Escolha errada. Tente um número menor: "))
+
+if ngerado == nescolhido:
+        print(f"Parabéns, você acerto!. O número certo é {ngerado}")
