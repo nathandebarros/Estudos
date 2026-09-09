f21f00d64eb37bb63c32e6056ec27dfa77e6598e
nathandebarros <nathanbartolomeu@gmail.com>
2026-09-09 14:20:33 -0300

    Update jogodavelha.py

--- jogodavelha.py
+++ jogodavelha.py
@@ -1,1 +1,14 @@
+#criando uma matriz que será a base para o jogo da velha
+matriz = [
+    [" "," "," "],
+    [" "," "," "],
+    [" "," "," "]
+]
 
+#definição da função responsavel por mostrar o corpo do jogo da velha
+def corpo(matriz):
+    print(f"{matriz[0][0]} | {matriz[0][1]} | {matriz[0][2]}")
+    print(f"{matriz[0][0]} | {matriz[0][1]} | {matriz[0][2]}")
+    print(f"{matriz[0][0]} | {matriz[0][1]} | {matriz[0][2]}")
+
+corpo(matriz)
