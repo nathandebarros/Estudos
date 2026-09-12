f75f13652ea04eccbc482a49f5539a87523a0165
nathandebarros <nathanbartolomeu@gmail.com>
2026-09-12 17:14:18 -0300

    Melhorando o código
    A função responsável por analisar as linhas estão funcionado, porém se ambos os jogadores ficarem por um espaço de ganhar em linhas diferentes, ambos ganham ao invés de só o primeiro. Além disso, falta ainda implementar a parte responsável por analisar as diagonais.

--- PedraPapelTesoura.py
+++ PedraPapelTesoura.py
@@ -1,4 +1,4 @@
-import random
+import random #importando a biblioteca responsável pela escolha do computador
 
 
 
--- jogodavelha.py
+++ jogodavelha.py
@@ -1,28 +1,97 @@
+#**ainda falta criar a função responsável por analisar as vitorias por diagonais**
+
 #criando uma matriz que será a base para o jogo da velha
 matriz = [
-    [" "," "," "],
-    [" "," "," "],
-    [" "," "," "]
+    [0, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0]
 ]
 
 #definição da função responsavel por mostrar o corpo do jogo da velha
 def corpo(matriz):
-    print(f"{matriz[0][0]} | {matriz[0][1]} | {matriz[0][2]}")
-    print(f"{matriz[1][0]} | {matriz[1][1]} | {matriz[1][2]}")
-    print(f"{matriz[2][0]} | {matriz[2][1]} | {matriz[2][2]}")
+    for i in matriz:
+        for j in i:
+            if j == 0:
+                print("| |", end="")
+            elif j == 1:
+                print("|X|", end="")
+            else:
+                print("|O|", end="")
+        print()
+
+#função responsável por registrar as jogadas dos jogadores
+def jogada():
+    OpcaoInvalida = False
+
+    X = input("X - Insira as coordenadas da sua jogada: ").split()
+    if ((0 <= int(X[0]) <=2 ) and (0 <= int(X[1]) <=2) and matriz[int(X[0])][int(X[1])] == 0):  #analisa se a coodenada esta dentro do index e se não está ocupada
+        matriz[int(X[0])][int(X[1])] = 1
+    else:
+        print("Opção invalida, tente novamente.")
+        OpcaoInvalida = True
+        while OpcaoInvalida:
+            X = input("X - Insira as coordenadas da sua jogada: ").split()
+            if ((0 <= int(X[0]) <=2 ) and (0 <= int(X[1]) <=2) and matriz[int(X[0])][int(X[1])] == 0):
+                matriz[int(X[0])][int(X[1])] = 1
+                OpcaoInvalida = False
+            else:
+                print("Opção invalida, tente novamente.")
+
+
+    O = input("O - Insira as coordenadas da sua jogada: ").split()
+    if ((0 <= int(O[0]) <=2 ) and (0 <= int(O[1]) <=2) and matriz[int(O[0])][int(O[1])] == 0):
+        matriz[int(O[0])][int(O[1])] = 2
+    else:
+        print("Opção invalida, tente novamente.")
+        OpcaoInvalida = True
+        while(OpcaoInvalida):
+            O = input("O - Insira as coordenadas da sua jogada: ").split()
+            if((0 <= int(O[0]) <=2 ) and (0 <= int(O[1]) <=2) and matriz[int(O[0])][int(O[1])] == 0):
+                matriz[int(O[0])][int(O[1])] = 2
+                OpcaoInvalida = False
+        
+
+def AnalisaLinha(matriz):
+    global Player1Venceu, Player2Venceu
+    Player1Venceu = False
+    Player2Venceu = False
+    for i in range(len(matriz)):
+        if matriz[i][0] == 1 and matriz[i][1] == 1 and matriz[i][2]== 1:
+            print("O jogador X venceu.")
+            Player1Venceu = True
+        elif matriz[i][0] == 2 and matriz[i][1] == 2 and matriz[i][2]== 2:
+            print("O jogador O venceu.")
+            Player2Venceu = True
+        
 
 #função resposável por registar as jogadas dos jogadores
-def jogada():
-    X = input("Digite as coordenadas: ").split()
-    matriz[int(X[0])][int(X[1])] = "X"
-    O = input("Digite as coordenadas: ").split()
-    matriz[int(X[0])][int(X[1])] = "O"
+print("-----------------------------------------\n\t      JOGO DA VELHA  \n-----------------------------------------")
+
+"""player1 = int(input("Digite 1 para escolher \"X\" ou 2 para \"O\": "))
+if player1 == 1:
+    player2 = 2
+else:
+    player2 = 1
+
+if player1 == 1:
+    print("Jogador 1: X\nJogador 2: O")
+else:
+    print("Jogador 1: O\nJogadro 2: X")"""
+
+continuar = True
+
+while continuar:
+    corpo(matriz)
+    jogada()
+    AnalisaLinha(matriz)
+    if Player1Venceu or Player2Venceu:
+        corpo(matriz)
+        continuar = False
 
 
-def ganhou(matriz):
-    for i in range(len(matriz)):
-        for c in range(len(matriz)):
-            if matriz[i][c] == matriz[i+1][c+1] == matriz[i+2][c+2]:
-                print(f"Parabéns! O {matriz[i][c]} Ganhou.")
-            elif matriz[i][c] == matriz[i+1][c] == matriz[i+2][c]:
-                print(f"Parabéns! O {matriz[i][c]} ganhou")
+
+
+
+
+    
+            
